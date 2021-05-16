b9107ed6171402206d376e9897260833593cb4e9
Batoolayyad <batoolayyad1996@gmail.com>
2021-05-16 23:42:12 +0300

    lab1

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class header extends React.Component{
+    render(){
+        return(
+            <header>
+                <h1>Horned Animals</h1>
+            </header>
+        )
+    }
+}
+export default header;
