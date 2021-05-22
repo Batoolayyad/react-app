1bec6760113e4aafa9a1fb45e4c93d2cbb23191c
Batoolayyad <batoolayyad1996@gmail.com>
2021-05-23 01:58:47 +0300

    finish lab4 last edite

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
 import React from 'react';
-import Header from './component/header';
-import Main from './component/main';
-import Footer from './component/footer';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
 import DataHorned from './asset/data';
-import SelectBeast from './component/selectBeast';
+import SelectBeast from './component/SelectBeast';
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       dataArray: DataHorned,
       element: {},
-      show : false
+      show: false
     }
   }
-  showModal=(parameter)=>{
-let chosenElement= DataHorned.find((element) =>{
-  if (element.title === parameter){
-    return element;
+  showModal = (parameter) => {
+    let chosenElement = DataHorned.find((element) => {
+      if (element.title === parameter) {
+        return element;
+      }
+    })
+
+    this.setState({
+      element: chosenElement,
+      show: true,
+    })
+
   }
-})
+  hideModal = () => {
+    this.setState({
+      show: false,
+    })
+  }
 
-this.setState({
-  element:chosenElement,
-  show: true,
-})
-
-}
-hideModal=()=>{
-  this.setState({
-show:false,
-})
-}
-  render(){
-    return(
+  render() {
+    return (
       <>
-        <Header/>
+        <Header />
         <Main
-        beastArry= {this.state.dataArray}
-        showModal={this.showModal}
-         />
+          beastArry={this.state.dataArray}
+          showModal={this.showModal}
+        />
         <SelectBeast
-        element={this.state.element}
-        hideModal={this.hideModal}
-        show={this.state.show}
-      />
-        <Footer/>
+          element={this.state.element}
+          hideModal={this.hideModal}
+          show={this.state.show}
+        />
+        <Footer />
       </>
     )
   }
